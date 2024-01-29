3d5cc142b62d1fd5b73753f43242ecfd9eb9354a
Sidharth Surya <imsidharthsurya@gmail.com>
2024-01-29 22:27:00 +0530

    filter top rated restaurant using local var.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,23 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurantData from "../utils/mockData";
+let temp=restaurantData
+console.log("initially temp is: ",temp)
 const Body=()=>{
     return (
         <div className="body">
             <div className="search">
+                <button className="filter" onClick={()=>{
+                    temp=restaurantData.filter((restaurant)=>{
+                        return restaurant.info.avgRating>4.3;
+                    })
+                    console.log("new temp is ",temp)
+                }}>Top Rated Restaurant</button>
                 <h3>Search Restaurant</h3>
             </div>
             <div className="restaurant-container">
                 {
                     //to write js
-                    restaurantData.map((restaurant)=>{
+                    temp.map((restaurant)=>{
                        return <RestaurantCard key={restaurant.info.id} {...restaurant.info}/>
                     })
                 }
