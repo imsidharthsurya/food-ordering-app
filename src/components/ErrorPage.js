6db04784e7aa4d131d3421079198cd452c059df1
Sidharth Surya <imsidharthsurya@gmail.com>
2024-02-03 09:07:35 +0530

    error message display using useRouteError hook

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,8 +1,13 @@
+import { useRouteError } from "react-router-dom";
 const ErrorPage=()=>{
+    const err=useRouteError();
+    console.log(err.data)
+    console.log(err.statusText)
     return (
         <div className="error">
             <h1>Oops! Something went wrong</h1>
             <h3>Currently we're unable to process your request</h3>
+            <p>{err.data}: {err.statusText}</p>
         </div>
     )
 }
