0115721c2d27a8a709efa43504888a722490b234
Sidharth Surya <imsidharthsurya@gmail.com>
2024-02-03 09:26:08 +0530

    used link to do client side routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import {LOGO_URL} from "../utils/constants"
 import { useState } from "react";
+import { Link } from "react-router-dom";
 const Header=()=>{
     const [btnName,setBtnName]=useState("Login")
     return (
@@ -9,9 +10,9 @@ const Header=()=>{
             </div>
             <div className="nav-bar">
                 <ul>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Contact Us</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About us</Link></li>
+                    <li><Link to="/contact">Contact Us</Link></li>
                     <li>Cart</li>
                     <button className="login" onClick={()=>{
                         if(btnName=="Login")
