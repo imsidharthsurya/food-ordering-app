5f595122e2a347208ff77e9d596947628a1f1764
Sidharth Surya <imsidharthsurya@gmail.com>
2024-02-08 00:02:54 +0530

    rating added in restaurant info page

--- src/components/RestaurantInfo.js
+++ src/components/RestaurantInfo.js
@@ -2,6 +2,7 @@ import {IMG_URL} from "../utils/constants"
 import RestaurantInfoShimmer from "./RestaurantInfoShimmer";
 import { useParams } from "react-router-dom";
 import useRestaurantInfo from "../utils/useRestaurantInfo";
+import star from "../img/star.png"
 
 const RestaurantInfo=()=>{
     const {resId}=useParams();//destructuring on the fly
@@ -25,10 +26,21 @@ const RestaurantInfo=()=>{
 
     return (
         <div className="restaurant-info-main-div w-[70%] mt-12 mx-auto">
-            <h2 className="rest-name font-bold text-xl">{restInfo[0].card.card.info.name}</h2>
-            <p className="rest-details text-sm font-light mt-2">{restInfo[0].card.card.info.cuisines.join(", ")}</p>
-            <p className="rest-details text-sm font-light">{restInfo[0].card.card.info.locality +", "+restInfo[0].card.card.info.sla.lastMileTravelString}</p>
-            {restInfo[0].card.card.info.feeDetails.message?<p className="rest-details text-sm font-light mt-4"><img src={IMG_URL+restInfo[0].card.card.info.feeDetails.icon} className="rest-delivery-logo w-6 inline"/>{" "+restInfo[0].card.card.info.feeDetails.message} </p>:null}
+            <div className="flex justify-between">
+                <div>
+                    <h2 className="rest-name font-bold text-xl">{restInfo[0].card.card.info.name}</h2>
+                    <p className="rest-details text-sm font-light mt-2">{restInfo[0].card.card.info.cuisines.join(", ")}</p>
+                    <p className="rest-details text-sm font-light">{restInfo[0].card.card.info.locality +", "+restInfo[0].card.card.info.sla.lastMileTravelString}</p>
+                    {restInfo[0].card.card.info.feeDetails.message?<p className="rest-details text-sm font-light mt-4"><img src={IMG_URL+restInfo[0].card.card.info.feeDetails.icon} className="rest-delivery-logo w-6 inline"/>{" "+restInfo[0].card.card.info.feeDetails.message} </p>:null}
+                </div>
+                <div className="inline-block p-2 border border-gray-300 border-solid h-20">
+                    <img src={star} className="inline-block w-5 relative bottom-0.5"/><span className="inline-block text-green-700 font-bold">&nbsp;{restInfo[0].card.card.info.avgRatingString}</span>
+                    <hr className="my-2"/>
+                    <p className="text-xs text-gray-500 font-semibold tracking-tighter">{restInfo[0].card.card.info.totalRatingsString}</p>
+                    
+                </div>
+            </div>
+            <hr className="mt-8"/>
             <h3 className="recommended-item text-lg mt-8 font-bold">Recommended {"("+restMenu.length+")"}</h3>
             <ul>
                  { 
