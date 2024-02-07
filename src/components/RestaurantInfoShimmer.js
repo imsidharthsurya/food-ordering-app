e5a3abd04d79d790eb0aa791a9f68658307e22e0
Sidharth Surya <imsidharthsurya@gmail.com>
2024-02-07 23:30:36 +0530

    shimmer styling done

--- src/components/RestaurantInfoShimmer.js
+++ src/components/RestaurantInfoShimmer.js
@@ -1,14 +1,14 @@
 const RestaurantInfoShimmer=()=>{
     return (
             <div className="rest-shimmer">
-                <div className="rest-shim1">
+                <div className="rest-shim1 h-60 w-96 bg-gray-100 mt-12 ml-40">
 
                 </div>
-                <div className="rest-shim2">
+                <div className="rest-shim2 w-80 h-3 bg-gray-100 mt-5 ml-40">
 
                 </div>
                 
-                    <div className="rest-shim3">
+                    <div className="rest-shim3 w-56 bg-gray-100 h-2 mt-4 ml-40">
 
                     </div>
                     
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,14 +1,14 @@
 const UUi=()=>{
-    return (<div className="shimmer-ui">
-    <div className="shimmer-img"></div>
-    <div className="shimmer-h1"></div>
-    <div className="shimmer-p"></div>
+    return (<div className="shimmer-ui m-5">
+    <div className="shimmer-img w-48 h-32 bg-gray-100"></div>
+    <div className="shimmer-h1 bg-gray-100 w-36 h-2 mt-4"></div>
+    <div className="shimmer-p bg-gray-100 w-28 h-2 mt-2"></div>
 </div>)
 }
 const Shimmer=()=>{
     var arr=[1,2,1,2,1,2,1,2,1,2];
     return (
-        <div className="main-shimmer">
+        <div className="main-shimmer flex flex-wrap mt-20 ml-8">
            {
             arr.map((elem,ind)=>{
                 return <UUi key={ind}/>
